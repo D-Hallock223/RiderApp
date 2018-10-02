f36e2a67d3ab7e0495db729a3dfdbf5a69a5c179
D-Hallock223 <dhallock223@gmail.com>
2018-10-02 16:55:50 +0000

    login

--- app/src/main/java/com/example/xhaxs/rider/MainActivity.java
+++ app/src/main/java/com/example/xhaxs/rider/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.xhaxs.rider;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentuser= FirebaseAuth.getInstance().getCurrentUser();
+        if(currentuser==null)
+        {
+            // user is not logged in
+            // send him to login page
+            Intent i=new Intent(MainActivity.this,LoginActivity.class);
+            startActivity(i);
+            finish();
+        }
     }
 }
